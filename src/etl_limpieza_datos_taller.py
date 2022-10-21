8cf78981ad463e916e18508a9e7e399c29438a04
alebaquero18 <alebaquero@gmail.com>
2022-10-21 12:06:04 -0500

    arreglo cod limieza datos

--- src/etl_limpieza_datos_taller.py
+++ src/etl_limpieza_datos_taller.py
@@ -5,43 +5,96 @@ import pandas as pd
 import os
 from pathlib import Path
 
-filename="llamadas123_julio_2022.csv"
-data_dir="raw"
-file_path=os.path.join(root_dir,"data", data_dir, filename)
-data=pd.read_csv(file_path, encoding="latin-1", sep=";")
+bucket = "gs://amaldonado_bucket_llamadas123"
 
-data.head()
+def get_data(filename):
+    
+#filename="llamadas123_julio_2022.csv"
+    data_dir="raw"
+    file_path=os.path.join(bucket,"data", data_dir, filename)
+    data=pd.read_csv(file_path, encoding="latin-1", sep=";")
+    data.head()
 
+    print('get_data')
+    print('La tabla contiene', data.shape[0], 'filas', data.shape[1], 'columnas')
+    return data
+
+def generate_report(data):
+    # Crear un diccionario vacio
+    dict_resumen = dict()
+
+    # loop para llenar el diccionario de columnas con valores unicos
+    for col in data.columns:
+        valores_unicos = data[col].unique()
+        n_valores = len(valores_unicos)
+        dict_resumen[col] = n_valores
+
+    reporte = pd.DataFrame.from_dict(dict_resumen, orient='index')
+    reporte.rename({0: 'Count'}, axis=1, inplace=True)
+
+    print('generate_report')
+    print(reporte.head())
+    return reporte
+
+def save_data(reporte, filename):
+    # Guardar la tabla:
+
+    out_name = 'resumen_' + filename
+    out_path = os.path.join(bucket, 'data', 'processed', out_name)
+    reporte.to_csv(out_path)
+    
+    print("Guardado en BQ")
+    # Guardar la tabla en BigQuery
+    reporte.to_gbq(destination_table="linen-synthesis-364223.llamadas_123",)
+
+def main():
+
+    filename = "llamadas123_julio_2022.csv"
+    data = get_data(filename)
+    reporte = generate_report(data)
+    save_data(reporte, filename)
+
+    print('DONE!!!')
+
+if __name__ == '__main__':
+    main()
+    
 # Registros duplicados
 
-print("forma incial", data.shape)
-data=data.drop_duplicates() 
-print("forma final", data.shape)
-data.info()
+    bucket = "gs://amaldonado_bucket_llamadas123"
+
+def get_data(filename):
+    data_dir="raw"
+    file_path=os.path.join(bucket,"data", data_dir, filename)
+    data=pd.read_csv(file_path, encoding="latin-1", sep=";")
+    print("forma incial", data.shape)
+    data=data.drop_duplicates() 
+    print("forma final", data.shape)
+    data.info()
 
 # cuente los diferentes valores
-data["UNIDAD"].value_counts(dropna=False)
+    data["UNIDAD"].value_counts(dropna=False)
 
 # reemplace en la columna unidad los nulos por SIN_DATO
-data["UNIDAD"].fillna("SIN_DATO").value_counts(dropna=False, normalize=False)
-data["UNIDAD"]=data["UNIDAD"].fillna("SIN_DATO")
-data.info()
+    data["UNIDAD"].fillna("SIN_DATO").value_counts(dropna=False, normalize=False)
+    data["UNIDAD"]=data["UNIDAD"].fillna("SIN_DATO")
+    data.info()
 
 # Manipule las fechas 
-col="FECHA_INICIO_DESPLAZAMIENTO_MOVIL"
-data[col].max()
+    col="FECHA_INICIO_DESPLAZAMIENTO_MOVIL"
+    data[col].max()
 
 # Aplique filtros
-val_min=data[col].min()
-data[data[col] != val_min]
+    val_min=data[col].min()
+    data[data[col] != val_min]
 
 # Datetime
-data[col]=pd.to_datetime(data[col], errors="coerce")
-data.info()
+    data[col]=pd.to_datetime(data[col], errors="coerce")
+    data.info()
 
-data["RECEPCION"]
-val_min=data[col].min()
-data[data[col] == val_min]
+    data["RECEPCION"]
+    val_min=data[col].min()
+    data[data[col] == val_min]
 
 from dateutil.parser import parse
 parse("08/02/2021")
@@ -49,50 +102,5 @@ def convertir_formato_fecha(str_fecha):
     val_datetime=parse(str_fecha, dayfirst=False)
     return val_datetime
 
-
-data=data.reset_index()
-
-
-lista_fechas=list()
-n_filas=data.shape[0] #len(data[RECEPCION])
-#n_filas=len(data["RECEPCION"])
-for i in range(0, n_filas):
-
-    str_fecha=data["RECEPCION"][i]
-    
-    try: #tráte de hacerlo o sino haga algo
-        val_datetime=convertir_formato_fecha(str_fecha=str_fecha)
-        lista_fechas.append(val_datetime)
-    except Exception as e:
-        #print(i, e)
-        lista_fechas.append(str_fecha)
-        continue
-
-lista_fechas
-
-data["RECEPCION_CORREGIDA"]=lista_fechas
-data
-
-data["RECEPCION_CORREGIDA"]=pd.to_datetime(data["RECEPCION_CORREGIDA"], errors="coerce")
-data
-
-# CONSISTENCIA DE DATOS
-
-data["EDAD"].value_counts(dropna=False)
-
-data["EDAD"].replace({"SIN_DATO":np.nan}).value_counts(dropna=False)
-data["EDAD"]=data["EDAD"].replace({"SIN_DATO":np.nan})
-
-x=np.nan
-f=lambda x: x if pd.isna(x) == True else int(x)
-f(x)
-
-data ["EDAD"]=data["EDAD"].apply(f)
-
-data.info()
-
-word="BigData   "
-print(word, word.lower(), word.upper(), word.lstrip() )
-data["LOCALIDAD"].apply(lambda x: x.strip()).value_counts()
-
+    data=data.reset_index()
 
